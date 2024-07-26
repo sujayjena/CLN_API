7724d4fa148985e2bd25ada93736ab499a20903b
nazbullah-netswift <nazbullah.netswift2021@gmail.com>
2024-07-26 16:02:12 +0530

    export

--- Presentation/Casa.API/Controllers/Admin/RatePerKMController.cs
+++ Presentation/Casa.API/Controllers/Admin/RatePerKMController.cs
@@ -4,6 +4,9 @@ using CLN.Application.Models;
 using CLN.Persistence.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
+using System.Globalization;
 
 namespace CLN.API.Controllers.Admin
 {
@@ -72,6 +75,81 @@ namespace CLN.API.Controllers.Admin
                 var vResultObj = await _ratePerKMRepository.GetRatePerKMById(Id);
                 _response.Data = vResultObj;
             }
+            return _response;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> ExportRatePerKMData()
+        {
+            _response.IsSuccess = false;
+            byte[] result;
+            int recordIndex;
+            ExcelWorksheet WorkSheet1;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var request = new BaseSearchEntity();
+
+            IEnumerable<RatePerKM_Response> lstSizeObj = await _ratePerKMRepository.GetRatePerKMList(request);
+
+            using (MemoryStream msExportDataFile = new MemoryStream())
+            {
+                using (ExcelPackage excelExportData = new ExcelPackage())
+                {
+                    WorkSheet1 = excelExportData.Workbook.Worksheets.Add("RatePerKM");
+                    WorkSheet1.TabColor = System.Drawing.Color.Black;
+                    WorkSheet1.DefaultRowHeight = 12;
+
+                    //Header of table
+                    WorkSheet1.Row(1).Height = 20;
+                    WorkSheet1.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    WorkSheet1.Row(1).Style.Font.Bold = true;
+
+                    WorkSheet1.Cells[1, 1].Value = "Distance (KM)";
+                    WorkSheet1.Cells[1, 2].Value = "Rate";
+                    WorkSheet1.Cells[1, 3].Value = "Vehicle Type";
+                    WorkSheet1.Cells[1, 4].Value = "Status";
+
+                    WorkSheet1.Cells[1, 5].Value = "CreatedDate";
+                    WorkSheet1.Cells[1, 6].Value = "CreatedBy";
+
+
+                    recordIndex = 2;
+
+                    foreach (var items in lstSizeObj)
+                    {
+                        WorkSheet1.Cells[recordIndex, 1].Value = items.Distance;
+                        WorkSheet1.Cells[recordIndex, 2].Value = items.Rate;
+                        WorkSheet1.Cells[recordIndex, 3].Value = items.VehicleType;
+                        WorkSheet1.Cells[recordIndex, 4].Value = items.IsActive == true ? "Active" : "Inactive";
+
+                        WorkSheet1.Cells[recordIndex, 5].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                        WorkSheet1.Cells[recordIndex, 5].Value = items.CreatedDate;
+                        WorkSheet1.Cells[recordIndex, 6].Value = items.CreatorName;
+
+                        recordIndex += 1;
+                    }
+
+                    WorkSheet1.Column(1).AutoFit();
+                    WorkSheet1.Column(2).AutoFit();
+                    WorkSheet1.Column(3).AutoFit();
+                    WorkSheet1.Column(4).AutoFit();
+                    WorkSheet1.Column(5).AutoFit();
+                    WorkSheet1.Column(6).AutoFit();
+
+                    excelExportData.SaveAs(msExportDataFile);
+                    msExportDataFile.Position = 0;
+                    result = msExportDataFile.ToArray();
+                }
+            }
+
+            if (result != null)
+            {
+                _response.Data = result;
+                _response.IsSuccess = true;
+                _response.Message = "Rate Per KM list Exported successfully";
+            }
+
             return _response;
         }
     }
--- Presentation/Casa.API/Controllers/Admin/TicketsController.cs
+++ Presentation/Casa.API/Controllers/Admin/TicketsController.cs
@@ -4,6 +4,9 @@ using CLN.Application.Models;
 using CLN.Persistence.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml.Style;
+using OfficeOpenXml;
+using System.Globalization;
 
 namespace CLN.API.Controllers.Admin
 {
@@ -142,6 +145,78 @@ namespace CLN.API.Controllers.Admin
             return _response;
         }
 
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> ExportTicketStatusData()
+        {
+            _response.IsSuccess = false;
+            byte[] result;
+            int recordIndex;
+            ExcelWorksheet WorkSheet1;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var request = new BaseSearchEntity();
+
+            IEnumerable<TicketStatus_Response> lstSizeObj = await _ticketsRepository.GetTicketStatusList(request);
+
+            using (MemoryStream msExportDataFile = new MemoryStream())
+            {
+                using (ExcelPackage excelExportData = new ExcelPackage())
+                {
+                    WorkSheet1 = excelExportData.Workbook.Worksheets.Add("TicketStatus");
+                    WorkSheet1.TabColor = System.Drawing.Color.Black;
+                    WorkSheet1.DefaultRowHeight = 12;
+
+                    //Header of table
+                    WorkSheet1.Row(1).Height = 20;
+                    WorkSheet1.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    WorkSheet1.Row(1).Style.Font.Bold = true;
+
+                    WorkSheet1.Cells[1, 1].Value = "Ticket Status";
+                    WorkSheet1.Cells[1, 2].Value = "Sequence";
+                    WorkSheet1.Cells[1, 3].Value = "Status";
+
+                    WorkSheet1.Cells[1, 4].Value = "CreatedDate";
+                    WorkSheet1.Cells[1, 5].Value = "CreatedBy";
+
+
+                    recordIndex = 2;
+
+                    foreach (var items in lstSizeObj)
+                    {
+                        WorkSheet1.Cells[recordIndex, 1].Value = items.TicketStatus;
+                        WorkSheet1.Cells[recordIndex, 2].Value = items.SequenceNo;
+                        WorkSheet1.Cells[recordIndex, 3].Value = items.IsActive == true ? "Active" : "Inactive";
+
+                        WorkSheet1.Cells[recordIndex, 4].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                        WorkSheet1.Cells[recordIndex, 4].Value = items.CreatedDate;
+                        WorkSheet1.Cells[recordIndex, 5].Value = items.CreatorName;
+
+                        recordIndex += 1;
+                    }
+
+                    WorkSheet1.Column(1).AutoFit();
+                    WorkSheet1.Column(2).AutoFit();
+                    WorkSheet1.Column(3).AutoFit();
+                    WorkSheet1.Column(4).AutoFit();
+                    WorkSheet1.Column(5).AutoFit();
+
+                    excelExportData.SaveAs(msExportDataFile);
+                    msExportDataFile.Position = 0;
+                    result = msExportDataFile.ToArray();
+                }
+            }
+
+            if (result != null)
+            {
+                _response.Data = result;
+                _response.IsSuccess = true;
+                _response.Message = "Ticket Status list Exported successfully";
+            }
+
+            return _response;
+        }
+
         #endregion
 
         #region Ticket Type
@@ -258,6 +333,90 @@ namespace CLN.API.Controllers.Admin
                 var vResultObj = await _ticketsRepository.GetTicketStatusMatrixById(Id);
                 _response.Data = vResultObj;
             }
+            return _response;
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> ExportTicketStatusMatrixData()
+        {
+            _response.IsSuccess = false;
+            byte[] result;
+            int recordIndex;
+            ExcelWorksheet WorkSheet1;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var request = new BaseSearchEntity();
+
+            IEnumerable<TicketStatusMatrix_Response> lstSizeObj = await _ticketsRepository.GetTicketStatusMatrixList(request);
+
+            using (MemoryStream msExportDataFile = new MemoryStream())
+            {
+                using (ExcelPackage excelExportData = new ExcelPackage())
+                {
+                    WorkSheet1 = excelExportData.Workbook.Worksheets.Add("TicketStatusMatrix");
+                    WorkSheet1.TabColor = System.Drawing.Color.Black;
+                    WorkSheet1.DefaultRowHeight = 12;
+
+                    //Header of table
+                    WorkSheet1.Row(1).Height = 20;
+                    WorkSheet1.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    WorkSheet1.Row(1).Style.Font.Bold = true;
+
+                    WorkSheet1.Cells[1, 1].Value = "Ticket Status";
+                    WorkSheet1.Cells[1, 2].Value = "Ticket Priority";
+                    WorkSheet1.Cells[1, 3].Value = "Sequence";
+                    WorkSheet1.Cells[1, 4].Value = "SLA Days";
+                    WorkSheet1.Cells[1, 5].Value = "SLA Hrs";
+                    WorkSheet1.Cells[1, 6].Value = "SLA Min";
+                    WorkSheet1.Cells[1, 7].Value = "Status";
+
+                    WorkSheet1.Cells[1, 8].Value = "CreatedDate";
+                    WorkSheet1.Cells[1, 9].Value = "CreatedBy";
+
+
+                    recordIndex = 2;
+
+                    foreach (var items in lstSizeObj)
+                    {
+                        WorkSheet1.Cells[recordIndex, 1].Value = items.TicketStatus;
+                        WorkSheet1.Cells[recordIndex, 2].Value = items.TicketCategory;
+                        WorkSheet1.Cells[recordIndex, 3].Value = items.SequenceNo;
+                        WorkSheet1.Cells[recordIndex, 4].Value = items.SLADays;
+                        WorkSheet1.Cells[recordIndex, 5].Value = items.SLAHours;
+                        WorkSheet1.Cells[recordIndex, 6].Value = items.SLAMin;
+                        WorkSheet1.Cells[recordIndex, 7].Value = items.IsActive == true ? "Active" : "Inactive";
+
+                        WorkSheet1.Cells[recordIndex, 8].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                        WorkSheet1.Cells[recordIndex, 8].Value = items.CreatedDate;
+                        WorkSheet1.Cells[recordIndex, 9].Value = items.CreatorName;
+
+                        recordIndex += 1;
+                    }
+
+                    WorkSheet1.Column(1).AutoFit();
+                    WorkSheet1.Column(2).AutoFit();
+                    WorkSheet1.Column(3).AutoFit();
+                    WorkSheet1.Column(4).AutoFit();
+                    WorkSheet1.Column(5).AutoFit();
+                    WorkSheet1.Column(6).AutoFit();
+                    WorkSheet1.Column(7).AutoFit();
+                    WorkSheet1.Column(8).AutoFit();
+                    WorkSheet1.Column(9).AutoFit();
+
+                    excelExportData.SaveAs(msExportDataFile);
+                    msExportDataFile.Position = 0;
+                    result = msExportDataFile.ToArray();
+                }
+            }
+
+            if (result != null)
+            {
+                _response.Data = result;
+                _response.IsSuccess = true;
+                _response.Message = "Ticket Status Matrix list Exported successfully";
+            }
+
             return _response;
         }
 
