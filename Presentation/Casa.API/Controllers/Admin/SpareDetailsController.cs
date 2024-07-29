d64a86ac8c7afbab5ab561e62fd37110de007b24
nazbullah-netswift <nazbullah.netswift2021@gmail.com>
2024-07-29 15:39:49 +0530

    spare details export

--- Presentation/Casa.API/Controllers/Admin/SpareDetailsController.cs
+++ Presentation/Casa.API/Controllers/Admin/SpareDetailsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using System.Globalization;
 
 namespace CLN.API.Controllers.Admin
 {
@@ -230,6 +231,86 @@ namespace CLN.API.Controllers.Admin
             return result;
         }
 
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<ResponseModel> ExportSpareDetailsData()
+        {
+            _response.IsSuccess = false;
+            byte[] result;
+            int recordIndex;
+            ExcelWorksheet WorkSheet1;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var request = new SpareDetails_Search();
+            request.SpareCategoryId = 0;
+            IEnumerable<SpareDetails_Response> lstObj = await _spareDetailsRepository.GetSpareDetailsList(request);
+
+            using (MemoryStream msExportDataFile = new MemoryStream())
+            {
+                using (ExcelPackage excelExportData = new ExcelPackage())
+                {
+                    WorkSheet1 = excelExportData.Workbook.Worksheets.Add("SpareDetails");
+                    WorkSheet1.TabColor = System.Drawing.Color.Black;
+                    WorkSheet1.DefaultRowHeight = 12;
+
+                    //Header of table
+                    WorkSheet1.Row(1).Height = 20;
+                    WorkSheet1.Row(1).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                    WorkSheet1.Row(1).Style.Font.Bold = true;
+
+                    WorkSheet1.Cells[1, 1].Value = "Spare Category";
+                    WorkSheet1.Cells[1, 2].Value = "Spare Part Code";
+                    WorkSheet1.Cells[1, 3].Value = "Spare Description";
+                    WorkSheet1.Cells[1, 4].Value = "UOM";
+                    WorkSheet1.Cells[1, 5].Value = "Min Qty.";
+                    WorkSheet1.Cells[1, 6].Value = "Status";
+
+                    WorkSheet1.Cells[1, 7].Value = "CreatedDate";
+                    WorkSheet1.Cells[1, 8].Value = "CreatedBy";
+
+
+                    recordIndex = 2;
+
+                    foreach (var items in lstObj)
+                    {
+                        WorkSheet1.Cells[recordIndex, 1].Value = items.SpareCategory;
+                        WorkSheet1.Cells[recordIndex, 2].Value = items.UniqueCode;
+                        WorkSheet1.Cells[recordIndex, 3].Value = items.SpareDesc;
+                        WorkSheet1.Cells[recordIndex, 4].Value = items.UOMName;
+                        WorkSheet1.Cells[recordIndex, 5].Value = items.MinQty;
+                        WorkSheet1.Cells[recordIndex, 6].Value = items.IsActive == true ? "Active" : "Inactive";
+
+                        WorkSheet1.Cells[recordIndex, 7].Style.Numberformat.Format = DateTimeFormatInfo.CurrentInfo.ShortDatePattern;
+                        WorkSheet1.Cells[recordIndex, 7].Value = items.CreatedDate;
+                        WorkSheet1.Cells[recordIndex, 8].Value = items.CreatorName;
+
+                        recordIndex += 1;
+                    }
+
+                    WorkSheet1.Column(1).AutoFit();
+                    WorkSheet1.Column(2).AutoFit();
+                    WorkSheet1.Column(3).AutoFit();
+                    WorkSheet1.Column(4).AutoFit();
+                    WorkSheet1.Column(5).AutoFit();
+                    WorkSheet1.Column(6).AutoFit();
+                    WorkSheet1.Column(7).AutoFit();
+                    WorkSheet1.Column(8).AutoFit();
+
+                    excelExportData.SaveAs(msExportDataFile);
+                    msExportDataFile.Position = 0;
+                    result = msExportDataFile.ToArray();
+                }
+            }
+
+            if (result != null)
+            {
+                _response.Data = result;
+                _response.IsSuccess = true;
+                _response.Message = "Spare Details list Exported successfully";
+            }
+
+            return _response;
+        }
         #endregion
     }
 }
